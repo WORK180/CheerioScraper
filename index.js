ffd11b00a92750d6369c87df9e64baf06383eeb4
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-01-28 11:41:42 +1000

    Employer description added

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ for (i = 1; i < argv._.length; i++) {
 }
 var employerCriteria = {
     "employer": employerName.trim(),
-    "slug" : `${employer}`,
-    "criteria": []
+    "slug": `${employer}`,
+    "criteria": [],
+    "description": ""
 };
+
+var employerDetails = {
+
+}
 
 
 
@@ -26,12 +31,13 @@ request(`https://www.work180.com.au/clients/${employer}`, function (error, respo
             criteriaAnswer.push(a.text().trim());
             criteriaQuestion.push(q.text().trim());
         });
+        employerCriteria.description = $('#client-content').html();
     }
     for (i = 0; i < criteriaQuestion.length; i++) {
         var jsonElement = {
-            "question" : criteriaQuestion[i],
-            "answer" : criteriaAnswer[i]
-    };
+            "question": criteriaQuestion[i],
+            "answer": criteriaAnswer[i]
+        };
         employerCriteria.criteria.push(jsonElement);
     }
 
